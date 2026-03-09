7dd17e506d94c6b25096e43153940e0834308dea
Timo Frankrone <timorech13@hotmail.com>
2026-03-09 17:37:07 +0100

    Remove build folder and add gitignore

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_BodeViewer_FILE C:/Users/timor/Desktop/Studium/C++/Programm/BodeViewer/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/BodeViewer.exe)
-set(__QT_DEPLOY_TARGET_BodeViewer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_BodeViewer_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Charts.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
